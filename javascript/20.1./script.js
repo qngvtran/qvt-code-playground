8bc4e6334f9ffdcefa64f41786cdcb775413b3ef
Quang Vu Tran <qngvtran@gmail.com>
2026-01-20 15:46:38 +0100

    Improved If Else Logic

--- javascript/20.1./script.js
+++ javascript/20.1./script.js
@@ -4,12 +4,25 @@ const favPlayer = prompt(
   "Are you Ronaldo or Messi fan? (Enter their iconic number)",
 );
 
-let result = favPlayer;
+const msgRonaldo = "GOOD CHOICE!! THE REAL GOAT SUII";
+const msgMessi = "2nd best player.";
+const msgOther = "I said either 7 or 10..";
 
-if (isNaN(result)) {
-  result = "Enter a number";
-} else if (favPlayer === null || favPlayer === "") {
+let result;
+
+if (favPlayer === null || favPlayer.trim() === "") {
   result = "You forgot to type a number bruh";
-} else result;
+} else {
+  const playerNumber = Number(favPlayer);
 
+  if (isNaN(playerNumber)) {
+    result = "Enter a valid number";
+  } else if (playerNumber === 7) {
+    result = msgRonaldo;
+  } else if (playerNumber === 10) {
+    result = msgMessi;
+  } else {
+    result = msgOther;
+  }
+}
 console.log(result);

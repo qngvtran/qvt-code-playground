3cd37fd10f7e16d605c6420ad4bcac3d04d56481
Quang Vu Tran <qngvtran@gmail.com>
2026-01-20 16:03:24 +0100

    Comment: Code Understanding

--- javascript/20.1./script.js
+++ javascript/20.1./script.js
@@ -14,7 +14,6 @@ if (favPlayer === null || favPlayer.trim() === "") {
   result = "You forgot to type a number bruh";
 } else {
   const playerNumber = Number(favPlayer);
-
   if (isNaN(playerNumber)) {
     result = "Enter a valid number";
   } else if (playerNumber === 7) {
@@ -26,3 +25,13 @@ if (favPlayer === null || favPlayer.trim() === "") {
   }
 }
 console.log(result);
+
+// Code understanding:
+
+//It starts with the prompt.
+// And we move to the if statement.
+// The result serves as a storage for the result we get out of the if statement.
+// In the if statement if I forget to type a number or close it it will say a message which will get "stored" in variable and console log it.
+// However If i typed anything it has to be a number otherwise we get a message to enter a valid number.
+// If i get past this and actually typed a number, lets say either 7 or 10 we will get a message we set up earlier, msgRonaldo or msgMesssi.
+// If i didnt type any of these two numbers we will get a message from variable msgOther and with this the script ends.

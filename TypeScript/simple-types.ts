d1621ad92139740b71b57a62c5dfb43c665189d3
Quang Vu Tran <qngvtran@gmail.com>
2026-01-24 22:46:44 +0100

    Learning basic types

--- TypeScript/simple-types.ts
+++ TypeScript/simple-types.ts
@@ -1,12 +1,17 @@
-let jerseyNumber: number = 7;
-let username: string = "Quang";
-let isAdmin: boolean = true;
-let city: string = "Prague";
+// Simple types
+// First time using TypeScript
 
+let playerName: string = "Ronaldo";
+let playerAge: number;
+let jerseyNumber: number = 7;
+let isRetired: boolean = false;
+let homeCountry: string = "Lisbon";
+
+console.log(playerName);
+console.log(`Ronaldo is ${(playerAge = 40)} years old`);
 console.log(`Ronaldo wears number ${jerseyNumber}`);
-console.log(username);
-console.log(isAdmin);
-console.log(city);
+console.log(isRetired);
+console.log(homeCountry);
 
 const scores: number[] = [1, 2, 3];
 

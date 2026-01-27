2ad848264f4e13eee0502e1cecc9cf119e79f3ae
Quang Vu Tran <qngvtran@gmail.com>
2026-01-27 22:15:58 +0100

    enum types

---
+++ TypeScript/enum-types.ts
@@ -0,0 +1,54 @@
+enum Direction {
+  Up,
+  Right,
+  Down,
+  Left,
+}
+
+let moveUp: Direction = Direction.Up;
+let moveRight: Direction = Direction.Right;
+let moveDown: Direction = Direction.Down;
+let moveLeft: Direction = Direction.Left;
+
+console.log(moveUp);
+console.log(moveRight);
+console.log(moveDown);
+console.log(moveLeft);
+
+enum Status {
+  Pending = 5, // if the first value is assigned, the rest auto-increments, so Approved is gonna be 6, Rejected 7
+  Approved,
+  Rejected,
+}
+
+console.log(Status.Approved);
+console.log(Status.Rejected);
+
+enum Role {
+  Admin = "ADMIN",
+  User = "USER",
+  Guest = "GUEST",
+}
+
+let userRole: Role = Role.Admin;
+
+console.log(userRole);
+
+enum Sizes {
+  Small = 10,
+  Medium = Small * 1.5,
+  Large = Medium * 1.5,
+}
+
+console.log(Sizes.Small);
+console.log(Sizes.Medium);
+console.log(Sizes.Large);
+
+enum Color {
+  Red = 5,
+  Green,
+  Blue,
+}
+
+console.log(Color.Green);
+console.log(Color[7]);
